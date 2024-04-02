31427316d492a8dd35003ec21d0cb4e36b4995a8
EliteOneTube <manos1234599@gmail.com>
2024-04-03 00:14:28 +0300

    move button to position

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -47,6 +47,7 @@ export default async function Home() {
     return (
         <div className="container mt-8 mx-auto py-8 border border-gray-300 rounded-md shadow-md">
             <div className="flex justify-between mb-4">
+                <div></div>
                 <Link
                 href="/projects/createProject"
                 className="mr-8 py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
