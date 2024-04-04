95f50bd92d012397973915fe965904d2f229ce2e
EliteOneTube <manos1234599@gmail.com>
2024-04-05 00:03:41 +0300

    Remove search bar if no items to search provided

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -20,15 +20,17 @@ function SearchBar<T>({ items, endpoint}: SearchBarProps<T>) {
     };
 
     return (
-        <div className="flex justify-center items-center mb-4">
-            <input
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={handleChange}
-                className="w-1/2 p-2 border border-gray-300 rounded-md shadow-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            />
-        </div>
+        items.length > 0 && (
+            <div className="flex justify-center items-center mb-4">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleChange}
+                    className="w-1/2 p-2 border border-gray-300 rounded-md shadow-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                />
+            </div>
+        )
     );
 }
 
